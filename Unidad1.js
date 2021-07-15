d743c4b92f3478b320c1acaff5d54c478f536894
maria sol <poncesol97@hotmail.com>
2021-07-15 11:07:00 -0300

    primer repo

---
+++ Unidad1.js
@@ -0,0 +1,7 @@
+primeravariable = 40
+segundavariable = 16
+sumatotalvariable = primeravariable + segundavariable
+mensaje = "El resultado de la suma de las dos variables es :"
+
+document.write ("<h1>" + mensaje + Number(sumatotalvariable) + "</h1>")
+
